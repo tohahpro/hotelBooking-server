546df770cc88d930dc6e4346d745b2fdb0e35473
Toha810 <tohahpro@gmail.com>
2023-11-05 20:36:23 +0600

    room get by using id create

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require("cors")
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4100
+
+
+app.use(express.json());
+app.use(cors());
 
 
 
 
 
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jytf0dy.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +30,25 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const roomCollection = client.db('hotelDB').collection('allroom')
+
+        app.get('/rooms', async (req, res) => {
+            const cursor = roomCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        app.get('/rooms/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await roomCollection.findOne(query)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
